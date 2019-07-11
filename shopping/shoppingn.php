35263176a8a12ac4d7967c49f8f2524d2e756d65
josemunga <47523424+josemunga@users.noreply.github.com>
2019-07-11 13:39:33 +0300

    Add files via upload

---
+++ shopping/shoppingn.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<?php
+include('chunks/header.php');
+?>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <div class="rounded">
+    <main class="container clear"> 
+      <!-- main body --> 
+      <!-- ################################################################################################ -->
+      <div id="gallery">
+        <figure>
+          <header class="heading">Latest Images From The University</header>
+          <ul class="nospace clear">
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" ><img class="borderedbox" src="images/images/lecturers/comp/100_0190.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 3"><img class="borderedbox" src="images/images/lecturers/comp/100_0190.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 4"><img class="borderedbox" src="images/images/lecturers/comp/20151102_075956-1.jpg" alt=""></a></li>
+            <li class="one_quarter first"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 5"><img class="borderedbox" src="images/images/lecturers/comp/ben%20osero%20chuka%20university.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 6"><img class="borderedbox" src="images/images/lecturers/comp/COD%20COMP%20SCI.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 7"><img class="borderedbox" src="images/images/lecturers/comp/codscience.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 8"><img class="borderedbox" src="images/images/lecturers/comp/compsciictofficeschukauniversity.jpg" alt=""></a></li>
+            <li class="one_quarter first"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 9"><img class="borderedbox" src="images/images/lecturers/comp/computerlab3.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 10"><img class="borderedbox" src="images/images/lecturers/comp/internscomputer.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="../images/demo/slider/1.png" title="Display Text 11"><img class="borderedbox" src="images/images/lecturers/Dean%20of%20Students%20Chuka%20University.jpg" alt=""></a></li>
+            <li class="one_quarter"><a class="nlb" data-lightbox-gallery="gallery1" href="#" title="Display Text 12"><img class="borderedbox" src="images/images/lecturers/2015ChukaGraduationNewsletter.jpg" alt=""></a></li>
+          </ul>
+          <figcaption>Gallery Description Goes Here</figcaption>
+        </figure>
+      </div>
+      <!-- ################################################################################################ --> 
+      <!-- ################################################################################################ -->
+      <nav class="pagination">
+        <ul>
+          <li><a href="#">&laquo; Previous</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><strong>&hellip;</strong></li>
+          <li><a href="#">6</a></li>
+          <li class="current"><strong>7</strong></li>
+          <li><a href="#">8</a></li>
+          <li><a href="#">9</a></li>
+          <li><strong>&hellip;</strong></li>
+          <li><a href="#">14</a></li>
+          <li><a href="#">15</a></li>
+          <li><a href="#">Next &raquo;</a></li>
+        </ul>
+      </nav>
+      <!-- ################################################################################################ --> 
+      <!-- / main body -->
+      <div class="clear"></div>
+    </main>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<?php
+include('chunks/footer.php');
+?>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+</body>
+</html>
