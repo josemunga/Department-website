0781c54a1d81744dc90562af8c4e578fe530f217
josemunga <47523424+josemunga@users.noreply.github.com>
2019-07-11 13:18:13 +0300

    Assets

---
+++ assets/scripts/tuma.js
@@ -0,0 +1,7 @@
+function tuma(){
+	alert("thank you for registering with us! you'll now be redirected to the login page. please login with the newly acquired account");
+}
+
+function sandemwami(){
+alert("sande mwami kwa kutuma hizo vitu kwa DB ya search!")
+}
